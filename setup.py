f73cf35cb6164593b6836b75d6b2ebb4ea4806b1
Andrew Gallant <jamslam@gmail.com>
2013-08-14 16:05:24 -0400

    Include the module docstring as the long description in setup.py.

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
+import codecs
 from distutils.core import setup
 from glob import glob
 
-longdesc = \
-'''A simple program (and library) to auto generate API documentation for Python 
-libraries. All documentation text is parsed as Markdown and can be outputted as 
-HTML or as plain text for console use. There are no special syntax rules, just 
-pure Markdown.
-
-While pdoc tries to stay consistent with documentation practices recommend by 
-PEP 8 and PEP 257, pdoc also looks for documentation of representation in some 
-places just as epydoc does. Namely, docstrings proceeding module level 
-variables, class variables and instance variables in __init__ methods. This is 
-done by traversing the ast of the source text.'''
+longdesc = []
+with codecs.open('pdoc.py', 'r', 'utf-8') as f:
+    for i, line in enumerate(f):
+        if i == 0:
+            continue
+        if line.startswith('"""'):
+            break
+        longdesc.append(line)
+longdesc = ''.join(longdesc)
 
 install_requires = ['mako', 'markdown']
 try:  # Is this really the right way? Couldn't find anything better...
