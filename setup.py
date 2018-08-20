618cc48a12626d85843f91da2cc6a4163d194448
Erik Cederstrand <erik@cederstrand.dk>
2018-08-20 17:48:05 +0200

    Remove black which is Python 3.6-only
    It's not needed by the test suite anyway.

--- setup.py
+++ setup.py
@@ -48,7 +48,6 @@ setup(
     provides=["pdoc"],
     extras_require={
         "dev": {
-            "black",
             "flake8>=3.5, <3.6",
             "pytest>=3.3,<4",
             "pytest-cov>=2.5.1,<3",
