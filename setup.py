63a662222055d21dcf14ebebd48f5b5384271195
Maximilian Hils <git@maximilianhils.com>
2018-09-07 13:37:16 +0200

    fix version specification

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
-import codecs
+import os
+import re
 from distutils.core import setup
-import os.path as path
 
-cwd = path.dirname(__file__)
-longdesc = codecs.open(path.join(cwd, "longdesc.rst"), "r", "utf-8").read()
-version = "0.0.0"
-with codecs.open(path.join(cwd, "pdoc", "__init__.py"), "r", "utf-8") as f:
-    for line in f:
-        if line.startswith("__version__"):
-            exec(line.strip())
-            version = __version__
-            break
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "longdesc.rst"), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(os.path.join(here, "pdoc", "__init__.py")) as f:
+    VERSION = re.search(r'__version__ = "(.+?)"', f.read()).group(1)
 
 setup(
     name="pdoc",
     author="Andrew Gallant",
     author_email="pdoc@burntsushi.net",
-    version=version,
+    version=VERSION,
     license="UNLICENSE",
     description="A simple program and library to auto generate API "
     "documentation for Python modules.",
-    long_description=longdesc,
+    long_description=long_description,
     url="https://github.com/BurntSushi/pdoc",
     classifiers=[
         "Topic :: Documentation",
@@ -47,14 +45,18 @@ setup(
     entry_points={"console_scripts": ["pdoc = pdoc.cli:main"]},
     provides=["pdoc"],
     extras_require={
-        "dev": {
+        "dev": [
             "flake8>=3.5, <3.6",
             "pytest>=3.3,<4",
             "pytest-cov>=2.5.1,<3",
             "pytest-faulthandler>=1.3.1,<2",
             "pytest-timeout>=1.2.1,<2",
             "pytest-xdist>=1.22,<2",
-        }
+        ]
     },
-    install_requires=["mako", "markdown2", "pygments"],
+    install_requires=[
+        "mako>=1.0.7,<1.1",
+        "markdown2>=2.3.5,<2.4",
+        "pygments>=2.2.0,<2.3",
+    ],
 )
