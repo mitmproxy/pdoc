567e8966c76c8c04bc3574c38d33d985f9326ad6
Maximilian Hils <git@maximilianhils.com>
2021-02-01 14:15:30 +0100

    bump to 4.0.0

--- pdoc/__init__.py
+++ pdoc/__init__.py
@@ -280,7 +280,7 @@ We would be happy to accept contributions for Markdown and PDF.
 '''
 from __future__ import annotations
 
-__version__ = "3.0.1"  # this is read from setup.py
+__version__ = "4.0.0"  # this is read from setup.py
 
 import io
 import warnings
