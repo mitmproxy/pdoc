e59c3c6db65813770d386ff479ea9dc4ca8fcfef
Maximilian Hils <git@maximilianhils.com>
2021-05-27 23:21:57 +0200

    bump to 7.0.2

--- pdoc/__init__.py
+++ pdoc/__init__.py
@@ -340,7 +340,7 @@ We would be happy to accept contributions for Markdown and PDF.
 '''
 from __future__ import annotations
 
-__version__ = "7.0.1"  # this is read from setup.py
+__version__ = "7.0.2"  # this is read from setup.py
 
 import io
 import traceback
