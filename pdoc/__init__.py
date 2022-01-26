d0f7c4f145103840e165915639a9e9b22541ee7b
Maximilian Hils <git@maximilianhils.com>
2022-01-26 09:50:10 +0100

    bump to 9.0.1

--- pdoc/__init__.py
+++ pdoc/__init__.py
@@ -372,7 +372,7 @@ You can find an example in [`examples/library-usage`](https://github.com/mitmpro
 '''
 from __future__ import annotations
 
-__version__ = "9.0.0"  # this is read from setup.py
+__version__ = "9.0.1"  # this is read from setup.py
 
 import traceback
 import warnings
