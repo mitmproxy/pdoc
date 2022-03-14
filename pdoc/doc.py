ae271a94c89adf05236d91dc5a3949870e344b52
Jonathan Jin <me@jonathanj.in>
2022-03-14 12:52:22 -0400

    Document intended semantics of `.members`

--- pdoc/doc.py
+++ pdoc/doc.py
@@ -230,7 +230,11 @@ class Namespace(Doc[T], metaclass=ABCMeta):
 
     @cached_property
     def members(self) -> dict[str, Doc]:
-        """A mapping from all members to their documentation objects."""
+        """A mapping from all members to their documentation objects. This includes
+        'private' members; filtering is done elsewhere, e.g. at the templating
+        layer.
+
+        """
         members: dict[str, Doc] = {}
         for name, obj in self._member_objects.items():
             qualname = f"{self.qualname}.{name}".lstrip(".")
