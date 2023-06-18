4d28bd4d055a8e8ea72c5a51f092a207dc3cf2a0
Maximilian Hils <git@maximilianhils.com>
2023-06-18 19:10:42 +0200

    fixup

--- test/testdata/ast_parsing.py
+++ test/testdata/ast_parsing.py
@@ -1,15 +1,5 @@
-from typing import TypeVar
-
-T = TypeVar("T")
-
-
-def first(l: list[T]) -> T:
-    return l[0]
-
-
 class Foo:
     def __init__(self):
         self.no_docstring = 42
-
         self.with_docstring = 43
         """This is an attribute docstring."""
