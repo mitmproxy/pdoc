ba9b39ad4048e61a9c4c89a464eb06d8de71792b
Andy Abate <30606395+andyabateagility@users.noreply.github.com>
2023-12-01 22:04:51 -0800

    more markdown escaping examples (#642)

--- test/testdata/math_misc.py
+++ test/testdata/math_misc.py
@@ -1,18 +1,25 @@
+"""
+Markdown should ignore tokens within inline `$...$` and displaymath `$$...$$`.
+https://github.com/mitmproxy/pdoc/issues/639
+"""
+
 def test_stars():
     r"""
-    Markdown emphasis tokens (*) should be rendered correctly.
-    https://github.com/mitmproxy/pdoc/issues/639
+    Markdown emphasis tokens (`*`) should not be captured in math mode.
+
+    Currently broken: $*xyz*$
+
+    Workaround (escaping `*`): $\*xyz\*$
+
+    Workaround (extra whitespace): $* xyz *$
+
+    """
+
+def test_math_newline():
+    r"""
+    Markdown should not consume double backslashes (math newlines) in math mode.
 
     Currently broken:
-
-    $$
-    \begin{align*}
-    f(x) &= x^2\\
-         &= x \cdot x
-    \end{align*}
-    $$
-
-    Workaround:
 
     $$
     \begin{align\*}
@@ -20,5 +27,56 @@ def test_stars():
          &= x \cdot x
     \end{align\*}
     $$
+
+    Workaround (escaping `\\`):
+
+    $$
+    \begin{align\*}
+    f(x) &= x^2\\\\
+         &= x \cdot x
+    \end{align\*}
+    $$
     """
 
+def test_markdown_newline():
+    r"""
+    Markdown newlines (`\n\n`) should not emit a paragraph break in math mode.
+    
+    Currently broken:
+
+    $$
+    x + y
+
+    = z
+    $$
+
+    Workaround (no empty lines in math mode):
+
+    $$
+    x + y
+    % comment
+    = z
+    $$
+    """
+
+def test_macros():
+    r"""
+    Markdown should not capture headings (`#`) in math mode.
+    Currently broken:
+
+    $$
+    \newcommand{\define}[2]
+    {
+    #1 \quad \text{#2}
+    }
+    \define{e^{i\pi}+1=0}{Euler's identity}
+    $$
+
+
+    Workaround (no lines with leading `#`):
+
+    $$
+    \newcommand{\define}[2]{#1 \quad \text{#2}}
+    \define{e^{i\pi}+1=0}{Euler's identity}
+    $$
+    """
