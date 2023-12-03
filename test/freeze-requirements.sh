3941e9904f5f57b2aeb1551a1038583f3515ccb2
Maximilian Hils <git@maximilianhils.com>
2023-12-03 21:17:01 +0700

    update dependencies

--- test/freeze-requirements.sh
+++ test/freeze-requirements.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env sh
 set -ex
-cd -- "$(dirname -- "$0")"
+cd -- "$(dirname -- "$0")/.."
 
-rm -rf freeze-venv
-python3.8 -m venv freeze-venv
-freeze-venv/bin/python -m pip install -U pip
-freeze-venv/bin/pip install -e ..[dev]
-freeze-venv/bin/pip freeze --all --exclude-editable > ../requirements-dev.txt
-rm -rf freeze-venv
+pip-compile --all-extras --allow-unsafe --output-file=requirements-dev.txt pyproject.toml
