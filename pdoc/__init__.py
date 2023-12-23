5df57c78a147529f003e6a7d9bec2a930886c89a
Thorsten Meyer <thorsten.meyier@gmx.de>
2023-12-23 15:29:38 +0100

    explain @public

--- pdoc/__init__.py
+++ pdoc/__init__.py
@@ -180,9 +180,11 @@ ways.
 - If `__all__` is defined in the module, then all identifiers in that list will be considered public.
    No other identifiers will be considered public.
 - If `__all__` is not defined, then pdoc will consider all members public that
-   1. do not start with an underscore,
+   1. do not start with an underscore or have @public in their docstring,
    2. don't have `@private` in their docstring,
-   2. and are defined in the current module (i.e. they are not imported).
+   3. and are defined in the current module (i.e. they are not imported).
+
+
 
 In general, we recommend keeping these conventions:
 
