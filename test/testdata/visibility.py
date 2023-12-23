4ed4b8ff09419efd74eeae63e56c384fa7a57a3d
Thorsten Meyer <thorsten.meyier@gmx.de>
2023-12-23 16:15:14 +0100

    adapt visibility test script.

--- test/testdata/visibility.py
+++ test/testdata/visibility.py
@@ -1,5 +1,5 @@
 """
-Example where no children should be rendered.
+Example where only the children `__private_func` and  `__private_func_explicitly_public` should be rendered.
 """
 
 
@@ -11,4 +11,30 @@ class Undocumented:
 def my_func():
     """
     This is a public method that's not shown because it's marked as @private.
+    """
+
+
+def __protected_func():
+    """
+    This is a protected method that's not shown because its name starts with _.
+    """
+
+
+def __private_func():
+    """
+    This is a private method that's not shown because its name starts with __.
+    """
+
+
+def __private_func_explicitly_public():
+    """
+    This is a private method that's shown because it is explicitly marked
+    as public.
+    @public"""
+
+
+def my_other_func():
+    """
+    This is another public method that's shown. It should show without additional
+    whitespace above.
     """
