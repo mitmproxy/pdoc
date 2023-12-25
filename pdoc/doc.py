77ba2e970eeb8eb7d0aaf3b4f04390b8467cd1ed
Thorsten Meyer <thorsten.meyier@gmx.de>
2023-12-25 13:10:24 +0100

    get source_file of wrapped functions

--- pdoc/doc.py
+++ pdoc/doc.py
@@ -172,10 +172,7 @@ class Doc(Generic[T]):
     @cached_property
     def source_file(self) -> Path | None:
         """The name of the Python source file in which this object was defined. `None` for built-in objects."""
-        try:
-            return Path(inspect.getsourcefile(self.obj) or inspect.getfile(self.obj))  # type: ignore
-        except TypeError:
-            return None
+        return doc_ast.get_source_file(self.obj)
 
     @cached_property
     def source_lines(self) -> tuple[int, int] | None:
