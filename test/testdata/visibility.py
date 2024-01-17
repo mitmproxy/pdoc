6efdadc6a213b723308b4adfe5a6c52e69bf3e9f
tmeyier <95271082+tmeyier@users.noreply.github.com>
2024-01-17 22:01:13 +0100

    Update test/testdata/visibility.py

--- test/testdata/visibility.py
+++ test/testdata/visibility.py
@@ -42,9 +42,5 @@ def public_func():
     whitespace above.
     """
 
-
-def yet_another_func():
-    """
-    This is yet another public method to give a gauge for the expected amount of
     whitespace between too methods.
     """
