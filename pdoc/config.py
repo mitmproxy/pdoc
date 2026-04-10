38990ad9c5330053c3222f5cc540d1ae9e86cbfc
Chris Barnes <chris.barnes@gerbi-gmb.de>
2026-04-10 13:15:23 +0100

    Do not use `Self`

--- pdoc/config.py
+++ pdoc/config.py
@@ -13,7 +13,6 @@ from pathlib import Path
 from typing import Any
 from typing import BinaryIO
 from typing import Literal
-from typing import Self
 from typing import TypeVar
 
 import tomllib
@@ -100,7 +99,7 @@ class Config:
     """Don't open a browser after the web server has started."""
 
     @classmethod
-    def from_config_arg(cls, path: Path | None) -> Self:
+    def from_config_arg(cls, path: Path | None) -> Config:
         if path is None:
             return cls.from_toml_discover()
         elif path.is_file():
@@ -110,7 +109,7 @@ class Config:
         raise FileNotFoundError(f"Config path is not a file or directory: {path}")
 
     @classmethod
-    def _from_toml_dir(cls, path: Path) -> Self | None:
+    def _from_toml_dir(cls, path: Path) -> Config | None:
         """Load from a configuration file in the given path.
 
         Return None if none are found.
@@ -123,7 +122,7 @@ class Config:
         return None
 
     @classmethod
-    def from_toml_dir(cls, path: Path) -> Self:
+    def from_toml_dir(cls, path: Path) -> Config:
         """Load from a configuration file in the given path."""
         maybe = cls._from_toml_dir(path)
         if maybe is None:
@@ -131,7 +130,7 @@ class Config:
         return maybe
 
     @classmethod
-    def from_toml_discover(cls, path: Path | None = None) -> Self:
+    def from_toml_discover(cls, path: Path | None = None) -> Config:
         """Load from a configuration file in the given directory and its ancestors."""
         if path is None:
             path = Path.cwd()
@@ -148,7 +147,7 @@ class Config:
         return cls()
 
     @classmethod
-    def from_toml_path(cls, path: Path) -> Self:
+    def from_toml_path(cls, path: Path) -> Config:
         """Load configuration from the given file path."""
         pyproject = path.name == "pyproject.toml"
         logger.debug("Reading config from %s", path)
@@ -156,19 +155,19 @@ class Config:
             return cls.from_toml_file(f, pyproject)
 
     @classmethod
-    def from_toml_file(cls, f: BinaryIO, pyproject: bool = False) -> Self:
+    def from_toml_file(cls, f: BinaryIO, pyproject: bool = False) -> Config:
         """Load a configuration from the given binary IO (e.g. a file object in `"rb"` mode)."""
         table = tomllib.load(f)
         return cls.from_toml_table(table, pyproject)
 
     @classmethod
-    def from_toml_str(cls, toml: str, pyproject: bool = False) -> Self:
+    def from_toml_str(cls, toml: str, pyproject: bool = False) -> Config:
         """Load a configuration from a given string."""
         table = tomllib.loads(toml)
         return cls.from_toml_table(table, pyproject)
 
     @classmethod
-    def from_toml_table(cls, table: dict[str, Any], pyproject: bool = False) -> Self:
+    def from_toml_table(cls, table: dict[str, Any], pyproject: bool = False) -> Config:
         """Load a configuration from the given dict."""
         if pyproject:
             table = table.get("tool", dict())
